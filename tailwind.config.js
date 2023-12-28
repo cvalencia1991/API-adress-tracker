b9113db027c6e6a58b18b8b842597918803da0c3
Cesar Valencia <cesar4a6z@gmail.com>
2023-12-27 21:42:10 -0500

    Implement default CSS desing and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,23 @@ export default {
   "./index.html",
   "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "very_dark_gray": "hsl(0, 0%, 17%)",
+        "dark_gray": "hsl(0, 0%, 59%)",
+      },
+      fontFamily:{
+        "ribik": ["Ribik", "sans-serif"]
+      },
+      backgroundImage:{
+        "bgdesktop": "url('/src/img/pattern-bg-desktop.png')",
+        "bgmobile": "url('/src/img/pattern-bg-mobile.png')",
+      },
+      screens:{
+        "mobile": "375px",
+        "desktop": "1440px",
+      }
+    },
   },
   plugins: [],
 }
