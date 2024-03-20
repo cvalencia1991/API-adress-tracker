e73112a6abec8f6f0998f9fc6259bcdf1a4fcf02
cvalencia1991 <cesar4a6z@gmail.com>
2024-03-19 22:44:13 -0500

    Fix Design of information
    Upgrade Design of the Table and the structure of the components

--- src/components/InfoAddress.tsx
+++ src/components/InfoAddress.tsx
@@ -3,20 +3,20 @@
 const InfoAddress = () => {
 
   return (
-    <table className="flex flex-col justify-center relative bg-white rounded-2xl shadow-xl w-3/4 desktop:top-12 p-5 desktop:h-1/2 ">
+    <table className="flex text-xl flex-col justify-center relative bg-white rounded-2xl shadow-xl w-3/4 desktop:top-12 p-5 desktop:h-1/2 ">
       <tbody className="flex flex-col">
         <tr className="flex flex-col gap-1 desktop:flex-row justify-around items-center">
-          <td className="flex flex-col items-center desktop:items-start"><h2>IP ADDRESS</h2><p className="font-bold text-2xl">192.212.174.101</p></td>
+          <td className="flex flex-col items-center desktop:items-start"><h2 className="text-base">IP ADDRESS</h2><p className="font-bold ">192.212.174.101</p></td>
           <td><div className="none desktop:w-0.5 desktop:h-14 desktop:bg-gray-300"></div></td>
-          <td className="flex flex-col items-center desktop:items-start"><h2>LOCATION</h2><p className="font-bold text-2xl">Brooklyn, NY 10001</p></td>
+          <td className="flex flex-col items-center desktop:items-start"><h2 className="text-base">LOCATION</h2><p className="font-bold ">Brooklyn, NY 10001</p></td>
           <td><div className="none desktop:w-0.5 desktop:h-14 desktop:bg-gray-300"></div></td>
-          <td className="flex flex-col items-center desktop:items-start"><h2>TIMEZONE</h2><p className="font-bold text-2xl">UTC -05:00</p></td>
+          <td className="flex flex-col items-center desktop:items-start"><h2 className="text-base">TIMEZONE</h2><p className="font-bold ">UTC -05:00</p></td>
           <td><div className="none desktop:w-0.5 desktop:h-14 desktop:bg-gray-300"></div></td>
-          <td className="flex flex-col items-center desktop:items-start"><h2>ISP</h2><p className="font-bold text-2xl">SpaceX Starlink</p></td>
+          <td className="flex flex-col items-center desktop:items-start"><h2 className="text-base">ISP</h2><p className="font-bold ">SpaceX Starlink</p></td>
         </tr>
       </tbody>
     </table>
   )
 }
 
-export default InfoAddress
+export default InfoAddress;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,8 +5,8 @@ import SearchIp from "./SearchIp";
 const Navbar = () =>{
 
     return (
-        <div className="flex justify-center bg-bgmobile desktop:justify-around  bg-no-repeat bg-cover desktop:bg-bgdesktop w-full h-[20rem] ">
-            <div className="flex flex-col items-center justify-evenly h-full w-full mt-28 desktop:mt-7 gap-5">
+        <div className="flex justify-center contrast-100 bg-bgmobile desktop:justify-around  bg-no-repeat bg-cover desktop:bg-bgdesktop w-full  ">
+            <div className="flex relative flex-col items-center justify-evenly h-full w-full top-24 desktop:mt-7 gap-5 desktop:top-0 ">
                 <h1 className="font-ribik font-bold text-3xl text-white">IP Address Tracker </h1>
                     <SearchIp/>
                     <InfoAddress/>
