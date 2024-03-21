1a1ede7796e6329d1eff5e789ec1df78831c1c83
cvalencia1991 <cesar4a6z@gmail.com>
2024-03-20 22:38:01 -0500

    Setup Dispatching the function fetch Address
    - setup the information provider for the API fetch information
    that allows me to set default the state provide for the API

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,9 @@ import { Provider } from 'react-redux'
 import App from './App.tsx'
 import './index.css'
 import store from './features/store.ts'
+import { fetchIpAddress } from './features/reducers/ipReducer.ts'
+
+store.dispatch(fetchIpAddress({ipAddress:''}))
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store= {store}>
