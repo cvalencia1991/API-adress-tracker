ff66a070c55b1f301cd9f6e8e29004726d13e2dd
cvalencia1991 <cesar4a6z@gmail.com>
2024-03-23 17:09:08 -0500

    Implement types of the redux-thunk
    Implement the structure of the types that are going to handle when i use
    the API REST get information of the IP address tracker

---
+++ src/features/reducers/ipGeoLocationTypes.ts
@@ -0,0 +1,21 @@
+export interface IpAddressState {
+  ip: string | null;
+  location: {
+    country: string;
+    region: string;
+    city: string;
+    lat: number;
+    lng: number;
+    timezone: string;
+  } | null;
+  isp: string | null;
+  loading: boolean;
+}
+
+export interface FetchIpAddressArgs {
+  ipAddress: string ;
+}
+
+export interface rejectValue {
+  rejectValue:{ error:string }
+}
